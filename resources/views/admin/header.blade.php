2f1a926d68484dbe7b0f8868f2d6678ae992541f
R-Kourani <rola.kourani@hotmail.com>
2022-05-25 15:49:57 +0300

    Admin Panel Login Form & Login

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">CHARITY</a>
+            <a class="navbar-brand" href="{{route('admin.index')}}">My Admin Panel</a>
         </div>
 
         <div class="header-right">
---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Bootstrap Advance Admin Template</title>
+
+    <link href="{{asset('assets')}}/admin/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="{{asset('assets')}}/admin/css/font-awesome.css" rel="stylesheet" />
+    <!--CUSTOM BASIC STYLES-->
+    <link href="{{asset('assets')}}/admin/css/basic.css" rel="stylesheet" />
+    <!--CUSTOM MAIN STYLES-->
+    <link href="{{asset('assets')}}/admin/css/custom.css" rel="stylesheet" />
+
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body style="background-color: #E2E2E2;">
+<div class="container">
+    <div class="row text-center " style="padding-top:100px;">
+        <div class="col-md-12">
+            <img src="{{asset('assets')}}/admin/img/login.jpeg" style="width: 400px">
+        </div>
+    </div>
+    <div class="row ">
+
+        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+
+            <div class="panel-body">
+                @include('home.messages')
+                <form role="form" action="{{route('loginadmincheck')}}" method="post">
+                    <hr />
+                    <h5>Enter Details to Login</h5>
+                    <br />
+                    <div class="form-group input-group">
+                        <span class="input-group-addon"><i class="fa fa-tag"  ></i></span>
+                        <input type="email" class="form-control" placeholder="Email " />
+                    </div>
+                    <div class="form-group input-group">
+                        <span class="input-group-addon"><i class="fa fa-lock"  ></i></span>
+                        <input type="password" class="form-control"  placeholder="Password" />
+                    </div>
+                    <div class="form-group">
+                        <label class="checkbox-inline">
+                            <input type="checkbox" /> Remember me
+                        </label>
+                        <span class="pull-right">
+                                                   <a href="index.html" >Forget password ? </a>
+                        </span>
+                    </div>
+
+                    <a href="/admin" class="btn btn-primary ">Login</a>
+                    <hr />
+                    Not register ? <a href="/registeruser" >click here </a> or go to <a href="index.html">Home</a>
+                </form>
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
+
+
+<script src="{{asset('assets')}}/admin/js/jquery-1.10.2.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="{{asset('assets')}}/admin/js/bootstrap.js"></script>
+<!-- METISMENU SCRIPTS -->
+<script src="{{asset('assets')}}/admin/js/jquery.metisMenu.js"></script>
+<!-- CUSTOM SCRIPTS -->
+<script src="{{asset('assets')}}/admin/js/custom.js"></script>
+
+</body>
+</html>
